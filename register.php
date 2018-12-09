4b65372b964051f630a167f48ab183c8c7714d14
SsCq21 <43765572+SsCq21@users.noreply.github.com>
2018-12-09 16:21:28 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,35 +1,29 @@
 <?php
+$host="localhost"; // Host name 
+$username="root"; // Mysql username 
+$password="root"; // Mysql password 
+$db_name="isd"; // Database name 
 
-$username = filter_input(INPUT_POST, 'username');
-$email = filter_input(INPUT_POST, 'email');
-$password = filter_input(INPUT_POST, 'password');
+$conn=mysqli_connect("localhost:8889","root","root","isd");
+    if($conn->connect_error){
+        echo"$conn->connect_error";
+    }else{
+       // echo"ok";    
+    }
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];    
 if (!empty($username)){
   if (!empty($password)){
-
-    $host="localhost"; // Host name 
-    $username=""; // Mysql username 
-    $password=""; // Mysql password 
-    $db_name="isd"; // Database name 
-
-    // Create connection
-    $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-
-    if (mysqli_connect_error()){
-      die('Connect Error ('. mysqli_connect_errno() .') '
-        . mysqli_connect_error());
-    }
-    else{
-      $sql = "INSERT INTO user (username, email, password)
-      values ('$username','$email', '$password')";
+      $sql = "INSERT INTO user (username, email, password) values ('$username','$email', '$password')";
       if ($conn->query($sql)){
-        echo "New record is inserted sucessfully";
+        header("location: login.html");
       }
       else{
-        echo "Error: ". $sql ."
-    ". $conn->error;
-      }
+        echo "Error: ";
+        }
       $conn->close();
-    }
   }
   else{
     echo "Password should not be empty";
@@ -40,4 +34,5 @@ else{
   echo "Username should not be empty";
   die();
 }
+?>
 
